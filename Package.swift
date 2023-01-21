eef82a0794f87eb6d653e8cbcba14c39bf897f2d
Adrián Bolaños Ríos <abolanos@plainconcepts.com>
2023-01-21 15:06:59 +0100

    sdsdf

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkSPM",
-            dependencies: ["PackagePrueba2"]),
+            dependencies: ["PackagePrueba"]),
         .binaryTarget(
             name: "PackagePrueba2",
             path: "Sources/artifacts/PackagePrueba.xcframework"

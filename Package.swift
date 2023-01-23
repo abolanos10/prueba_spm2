4ecd20e5af5488fe0b5b3cf25bbcfc3859d10d22
Adrián Bolaños Ríos <abolanos@plainconcepts.com>
2023-01-23 10:00:05 +0100

    Kingfisher

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(
             name: "FrameworkSPM2",
            // type: .dynamic,
-            targets: ["FrameworkSPM2", "MobilePaymentSDK"]),
+            targets: ["FrameworkSPM2", "MobilePaymentSDK", "Kingfisher"]),
         //.library(name: "MobilePaymentSDK", targets: ["MobilePaymentSDK"])
     ],
     dependencies: [
